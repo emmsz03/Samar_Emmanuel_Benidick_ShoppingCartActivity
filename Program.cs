0e76a66822c695410922be3e9095960053dc2301
emmsz03 <emmsz010306@gmail.com>
2026-04-15 14:44:23 +0800

    Initial setup: created Product class and basic structure

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Product
+{
+    public int Id;
+    public string Name;
+    public double Price;
+    public int RemainingStock;
+
+    public void DisplayProduct()
+    {
+        Console.WriteLine($"{Id}. {Name} - ₱{Price} (Stock: {RemainingStock})");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Shopping Cart System");
+    }
+}
